5b7645ee708f04fad58a0897267a8e569c843ec4
Minh <minhvn29@gmail.com>
2020-07-04 18:17:40 -0700

    Create pagination for events

--- challenge_1/client/Components/App.jsx
+++ challenge_1/client/Components/App.jsx
@@ -9,11 +9,15 @@ class App extends React.Component {
     super();
     this.state = {
       searchInput: '',
-      results: []
+      results: [],
+      currentPage: 0,
+      pageCount: 0,
+      perPage: 10
     };
 
     this.dBSearch = this.dBSearch.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
 
 
   };
@@ -26,11 +30,13 @@ class App extends React.Component {
 
   
   dBSearch() {
-    axios.get(`/events?q=${this.state.searchInput}&_page=1&_limit=10`)
+    axios.get(`/events?q=${this.state.searchInput}&_page=${this.state.currentPage + 1}&_limit=${this.state.perPage}`)
       .then(response => {
-        console.log(response.data);
+        // console.log(response.headers);
         this.setState({
-          results: response.data
+          results: response.data, 
+          pageCount: Math.ceil(response.headers['x-total-count']/ this.state.perPage)
+
         });
       })
       .catch(err => {
@@ -38,12 +44,52 @@ class App extends React.Component {
       })
   };
 
+  handlePageClick(page) {
+    let selectedPage = page.selected;
+    this.setState({ currentPage: selectedPage}, () => {
+      this.dBSearch();
+    });
+  };
+
   render() {
     return(
       <div>
+
+        <div>
         <input type='text' onChange={this.handleInputChange}/>
         <input type='submit' onClick={this.dBSearch}/>
+        </div>
+        <br></br>
+        <h2>Events</h2>
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+        <div>
+        {
+          this.state.results.map((result, index) => {
+            return (
+              <div key={index} >
+                <div>{result.date}</div>
+                <div id={index}>{result.description}</div>
+                <br></br>
+              </div>
+            )
+          })
+        }
+        </div>
+
       </div>
+
     )
 
   };
